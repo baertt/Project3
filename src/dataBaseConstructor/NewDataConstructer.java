cece187a7b4fcdb0ffe981f3bc72108b4f063fd3
LiuYY3 <yiming.liu95@gmail.com>
2017-04-02 17:00:54 -0500

    real database works now

--- src/dataBaseConstructor/NewDataConstructer.java
+++ src/dataBaseConstructor/NewDataConstructer.java
@@ -1,5 +1,41 @@
 package dataBaseConstructor;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class NewDataConstructer {
+	public String newschedule(List<?> schedule) {
+		if (schedule.size() == 0) {
+			return "TBD";
+		} else {
+			HashMap<?, ?> sch_in = (HashMap<?, ?>) schedule.get(0);
+			String sch_out = sch_in.get("DayPattern").toString() + " "
+					+ checklength(sch_in.get("StartHour").toString()) + ":" + checklength(sch_in.get("StartMinute").toString()) + " - " 
+					+ checklength(sch_in.get("EndHour").toString()) + ":" + checklength(sch_in.get("EndMinute").toString()) + " "
+					+ sch_in.get("Building").toString() + " " + sch_in.get("Room").toString();
+			return sch_out;
+		}
+	}
 	
+	public String checklength(String string) {
+		if (string.length() < 2) {
+			string = "0" + string;
+			return string;
+		} else {
+			return string;
+		}
+	}
+	
+	public String instructor(List<?> instructor) {
+		String ins_out = "";
+		for (int i = 0; i < instructor.size(); i++) {
+			HashMap<?, ?> ins_in = (HashMap<?, ?>) instructor.get(i); 
+			if (i + 1 == instructor.size()) {
+				ins_out = ins_out + ins_in.get("LastName").toString() + ", " + ins_in.get("FirstName");
+			} else {
+				ins_out = ins_out + ins_in.get("LastName").toString() + ", " + ins_in.get("FirstName") + " & ";
+			}
+		}
+		return ins_out;
+	}
 }
